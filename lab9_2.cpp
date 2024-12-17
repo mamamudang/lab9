5e020063abc0a4e02e3316171e9952c2025e1fb3
mamamudang <humlake7.66@gmail.com>
2024-12-17 17:13:22 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int x, int y) {
+    if (x <= 0 || y <= 0) {
+        cout << "Invalid input\n"; 
+        return;
+    }
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            cout << 'O';
+        }
+        cout << '\n';
+    }
+}
 
 int main(){
 	
